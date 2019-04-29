b79d6de17f30c0a949b901e869660fc199ee352c
ljg <ljg2156@columbia.edu>
2019-04-28 21:35:51 -0400

    Create new df with all dates in range, merge that df with df w silverfish data to get a plot that's actually indicative of the fluctuations.

--- timeplot_silverfish.R
+++ timeplot_silverfish.R
@@ -7,6 +7,7 @@ library("foreign")
 library("dplyr")
 library("ggplot2")
 library("googlesheets")
+library("tidyverse")
  
 #' # Load in googlesheets data / clean up file
 silverfish_ws <- gs_title("silverfish_dat")
@@ -16,13 +17,32 @@ silverfish_dat <- gs_read(ss = silverfish_ws)
 silverfish_df <- as.data.frame(silverfish_dat)
 
 silverfish_df <- silverfish_df %>%
-  dplyr::mutate(date_formatted = as.Date(silverfish_df$date, "%m/%d/%Y"))
+  # cannot plot because dates are characters, must convert
+  dplyr::mutate(date_formatted = as.Date(silverfish_df$date, "%m/%d/%Y")) %>%
+  dplyr::group_by(date_formatted, num) %>%
+  # when plotting, was only showing 1s, wasn't counting occurrences so need to also create new column with counts 
+  dplyr::mutate(daily_count = n())
+
+# create df with 0s for complete dates
+full_dates <- seq(min(silverfish_df$date_formatted), max(silverfish_df$date_formatted), 
+                  by = "1 day")
+full_dates <- data.frame(date_formatted = full_dates,
+                         # made everything nans, so have to figure out how to insert 0s
+                         size = 0,
+                         daily_count = 0)
+
+## merge dfs 
+full_sf_df <- full_join(silverfish_df, full_dates,
+                          all.x = FALSE)
 
 #' # Plot timecourse occurrences
-ggplot(silverfish_df, aes(x = date_formatted, y = num)) +
+ggplot(full_sf_df, aes(x = date_formatted, y = daily_count)) +
   geom_line()
 
 #' # Plot timecourse occurrences w/ vertical lines for traps, DE in diff colors
+ggplot(silverfish_df, aes(x = date_formatted, y = daily_count)) +
+  geom_line() + 
+  geom_vline(xintercept = 0019-02-19, "red") 
 
 #' # Plot timecourse occurrences w/ vertical lines for traps, DE in diff colors, and change mancer size based on bug size
 
